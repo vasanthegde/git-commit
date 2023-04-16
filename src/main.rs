13d89d77ce185689e5f215cc4bdd10f09deb2969
Vasant Hegde <hegdevasanth1995@gmail.com>
2023-04-16 19:18:58 +0530

    add intial code

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::{
+    env,
+    process::{Command, Output},
+};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let args: &[String] = &args[1..];
+
+    let output: Output = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(args.first().expect("commit message required"))
+        .output()
+        .expect("Failed to run git commit command");
+
+    let dirty_out = String::from_utf8_lossy(&output.stdout);
+    println!("{}", dirty_out);
+    println!("Hello");
+}
